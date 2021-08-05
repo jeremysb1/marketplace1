70a9cdf71496d3b781a25f30ef165011e5214b74
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-08-05 14:55:30 -0400

    adding logo in container

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,13 @@ function Header() {
   return (
 	<header>
 	  {/* left */}
-	  <div>
-	  	<Image src="https://links.papareact.com/qd3" layout="fill" />
+	  <div className="relative flex items-center h-10 cursor-pointer my-auto">
+	  	<Image 
+	  	  src="https://links.papareact.com/qd3" 
+	  	  layout="fill"
+	  	  objectFit="contain"
+	  	  objectPosition="left"
+	  	/>
 	  </div>
 
 	  {/* middle */}
