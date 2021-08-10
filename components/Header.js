c1d1f137850d009d761801613052509ae6deb036
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-08-10 14:06:58 -0400

    adding menu and usericons and styling

--- components/Header.js
+++ components/Header.js
@@ -33,7 +33,12 @@ function Header() {
 	  {/* right */}
 	  <div className="flex items-center space-x-4 justify-end text-gray-500">
 	  	<p>Become a Host</p>
-	  	<GlobeAltIcon className="h-6" />
+	  	<GlobeAltIcon className="h-6 cursor-pointer" />
+
+	  	<div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+	  		<MenuIcon className="h-6" />
+	  		<UserCircleIcon className="h-6" />
+	  	</div>
 	  </div>
 	</header>
   )
