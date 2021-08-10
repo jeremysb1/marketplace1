5bfc171b4fd7bc4c10f3813862d5ff9cfec84e1d
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-08-10 14:53:05 -0400

    adding hero icon/banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 function Banner() {
   return (
-  	<div>
-  	  <h1>I am banner</h1>
+  	<div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+  	  <Image 
+  	    src="https://links.papareact.com/0fm"
+  	    layout="fill"
+  	    objectFit="cover"
+  	  />
   	</div>
   )
 }
