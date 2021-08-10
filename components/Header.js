16fd153c85acb7cf23d9ac4ed71d84c0990ec466
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-08-10 13:39:21 -0400

    add styling to search icon and text

--- components/Header.js
+++ components/Header.js
@@ -15,13 +15,13 @@ function Header() {
 	  </div>
 
 	  {/* middle - search */}
-	  <div className="flex items-center md:border-2 rounded-full py-2">
+	  <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
 	  	<input 
-	  	  className="pl-5 bg-transparent outline-none" 
+	  	  className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray" 
 	  	  type="text" 
 	  	  placeholder="start your search"
 	  	/>
-	  	<SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
+	  	<SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
 	  </div>
 
 	  {/* right */}
