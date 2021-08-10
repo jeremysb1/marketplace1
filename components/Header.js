9163fbc144485d9a75c56c2469bc34cfbd567c04
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-08-10 14:09:30 -0400

    making header text and icon mobile responsive

--- components/Header.js
+++ components/Header.js
@@ -32,7 +32,7 @@ function Header() {
 
 	  {/* right */}
 	  <div className="flex items-center space-x-4 justify-end text-gray-500">
-	  	<p>Become a Host</p>
+	  	<p className="hidden md:inline">Become a Host</p>
 	  	<GlobeAltIcon className="h-6 cursor-pointer" />
 
 	  	<div className="flex items-center space-x-2 border-2 p-2 rounded-full">
