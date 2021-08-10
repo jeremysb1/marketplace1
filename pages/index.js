06a36d4a601053843a7f224ade2f0582cc730268
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-08-10 16:23:56 -0400

    adding comment about upcoming data for card section

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,8 @@ export default function Home() {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+        {/* pull data from server - API endpoints */}
         </section>
       </main>
     </div>
