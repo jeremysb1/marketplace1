4e0fc91dd0d3b537287a988a708438ac2fca2a6c
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-08-11 15:35:20 -0400

    creating MediumCard

---
+++ components/MediumCard.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+function MediumCard({ img, title }) {
+  return (
+  	<div>
+  	  <div className="relative">
+  	  	<Image src={img} layout="fill" />
+  	  </div>
+  	</div>
+  );
+}
+
+export default MediumCard;
