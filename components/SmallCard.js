1f3bd786e6f26256b460fdc0b645dd07a19327db
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-08-11 12:40:57 -0400

    adding images to SmallCard function

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,7 +1,14 @@
-function SmallCard() {
+import Image from "next/image";
+
+function SmallCard({img, location, distance}) {
   return(
   	<div>
-  		
+  	  {/* Left */}
+  	  	<div className="relative h-16 w-16">
+  	  	  <Image src={img} layout="fill" className="rounded-lg"/>
+  	  	</div>
+  	  {/* Right */}
+
   	</div>
   )
 }
